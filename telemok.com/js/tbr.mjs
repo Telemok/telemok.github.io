b3754ddd53b843b7a6482c6ea24e34c4ab0a0b97
Telemok <tlevelpack@yandex.ru>
2021-04-14 00:17:26 +0700

    ok

---
+++ telemok.com/js/tbr.mjs
@@ -0,0 +1,45 @@
+/*
+* Copyright Telemok.com
+* */
+
+/*
+example:
+//import {tbr} from "https://telemok.github.io/telemok.com/js/tbr.mjs"
+//import {tbr} from "https://cdn.jsdelivr.net/gh/telemok/telemok.github.io@master/telemok.com/js/tbr.mjs"
+*/
+
+import {tbr as tbr1} from "https://cdn.jsdelivr.net/gh/telemok/com.telemok@27fd8b4/js/tbr.BitBuffer.mjs"
+import {tbr as tbr2} from "https://cdn.jsdelivr.net/gh/telemok/com.telemok@27fd8b4/js/tbr.assert.mjs"
+import {tbr as tbr3} from "https://cdn.jsdelivr.net/gh/telemok/com.telemok@27fd8b4/js/tbr.convert.mjs"
+import {tbr as tbr4} from "https://cdn.jsdelivr.net/gh/telemok/com.telemok@27fd8b4/js/tbr.html.mjs"
+import {tbr as tbr5} from "https://cdn.jsdelivr.net/gh/telemok/com.telemok@27fd8b4/js/tbr.time.mjs"
+import {tbr as tbr6} from "https://cdn.jsdelivr.net/gh/telemok/com.telemok@27fd8b4/js/tbr.request.mjs"
+var tbr = {...tbr1, ...tbr2, ...tbr3, ...tbr4, ...tbr5, ...tbr6}
+export {tbr};
+
+
+tbr.parseLocationHref = (queryString = location.href) =>
+{
+	tbr.assert.string(queryString);
+	if(queryString.indexOf('?') >= 0)
+		queryString = queryString.substr(queryString.indexOf('?')+1);
+	let pairs = queryString.split('&');
+	let result = {};
+	for (let i in pairs)
+	{
+		let pair = pairs[i].split('=');
+		let key = decodeURIComponent(pair[0]);
+
+		let value = typeof pair[1] === 'undefined' ? undefined : decodeURIComponent(pair[1]);
+		if(key.substr(-2,2) === '[]')
+		{
+			key = key.substr(0, key.length - 2);
+			if(!Array.isArray(result[key]))
+				result[key] = [];
+			result[key].push(value);
+		}
+		else
+			result[key] = value;
+	}
+	return result;
+}
