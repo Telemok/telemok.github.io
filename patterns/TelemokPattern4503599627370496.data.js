ba8a618d1671ab4fe367bb8f022cd71ca28fccef
Telemok <tlevelpack@yandex.ru>
2021-06-13 02:52:38 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370496.data.js
@@ -0,0 +1,42 @@
+/* telemok.com/patterns/pattern4503599627370496/ */
+export var telemokPatternsData = new Map([
+	[4503599627370496,{json:`{
+    \"patternType\": \"enum\",
+    \"patternBits\": 3,
+    \"patternName\": \"BurningStatus\",
+    \"patternVersion\": 1,
+    \"patternDescription\": \"generated enum\",
+    \"enumPrefix\": \"BurningStatus\",
+    \"enumItems\": [
+        {
+            \"enumItemRaw\": 7,
+            \"enumItemName\": \"ERROR\",
+            \"enumItemDescription\": \"\"
+        },
+        {
+            \"enumItemRaw\": 0,
+            \"enumItemName\": \"WAITING\",
+            \"enumItemDescription\": \"\\u043e\\u0436\\u0438\\u0434\\u0430\\u043d\\u0438\\u0435\"
+        },
+        {
+            \"enumItemRaw\": 1,
+            \"enumItemName\": \"IGNITION\",
+            \"enumItemDescription\": \"\\u0440\\u043e\\u0437\\u0436\\u0438\\u0433\"
+        },
+        {
+            \"enumItemRaw\": 2,
+            \"enumItemName\": \"COMBUSTION\",
+            \"enumItemDescription\": \"\\u0433\\u043e\\u0440\\u0435\\u043d\\u0438\\u0435\"
+        },
+        {
+            \"enumItemRaw\": 3,
+            \"enumItemName\": \"ATTENUATION\",
+            \"enumItemDescription\": \"\\u0432\\u044b\\u0436\\u0438\\u0433\\u0430\\u043d\\u0438\\u0435\"
+        },
+        {
+            \"enumItemRaw\": 4,
+            \"enumItemName\": \"NOIGNITION\",
+            \"enumItemDescription\": \"\\u043d\\u0435\\u0440\\u043e\\u0437\\u0436\\u0438\\u0433\"
+        }
+    ]
+}`}]]);
