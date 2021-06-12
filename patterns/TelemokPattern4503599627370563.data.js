86fbc492c5ee4b4d5ef66f42dbb96f015038dea6
Telemok <tlevelpack@yandex.ru>
2021-06-13 00:40:41 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370563.data.js
@@ -0,0 +1,24 @@
+/* telemok.com/patterns/pattern4503599627370563/ */
+export var telemokPatternsData = new Map([,
+	[4503599627370563,{json:`{
+    \"patternVersion\": 1,
+    \"patternName\": \"MoveDirection\",
+    \"patternBits\": 1,
+    \"patternDescription\": \"\\u041e\\u043f\\u0438\\u0441\\u0430\\u043d\\u0438\\u0435 \\u0434\\u0432\\u0438\\u0436\\u0435\\u043d\\u0438\\u044f \\u0432\\u043f\\u0435\\u0440\\u0451\\u0434 \\u0438\\u043b\\u0438 \\u043d\\u0430\\u0437\\u0430\\u0434, \\u043d\\u0430\\u043f\\u0440\\u0438\\u043c\\u0435\\u0440, \\u0434\\u043b\\u044f \\u043a\\u043e\\u043b\\u043e\\u0441\\u043d\\u0438\\u043a\\u0430.\",
+    \"patternType\": \"enum\",
+    \"enumSigned\": 0,
+    \"enumPrefix\": \"MOVE_DIRECTION_\",
+    \"enumRawDefault\": 1,
+    \"enumItems\": [
+        {
+            \"enumItemRaw\": 0,
+            \"enumItemName\": \"FORWARD\",
+            \"enumItemDescription\": \"\\u0414\\u0432\\u0438\\u0433\\u0430\\u0442\\u044c \\u0432\\u043f\\u0435\\u0440\\u0451\\u0434.\"
+        },
+        {
+            \"enumItemRaw\": 1,
+            \"enumItemName\": \"BACKWARD\",
+            \"enumItemDescription\": \"\\u0414\\u0432\\u0438\\u0433\\u0430\\u0442\\u044c \\u043d\\u0430\\u0437\\u0430\\u0434.\"
+        }
+    ]
+}`}]]);
