8cabab6c9deece50ecb5a874c85213339b8fdd86
Telemok <tlevelpack@yandex.ru>
2021-06-13 01:19:15 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370638.data.js
@@ -0,0 +1,31 @@
+/* telemok.com/patterns/pattern4503599627370638/ */
+export var telemokPatternsData = new Map([,
+	[4503599627370638,{json:`{
+    \"patternType\": \"struct\",
+    \"patternVersion\": 1,
+    \"patternBits\": 256,
+    \"patternName\": \"ThingToServerHashResponse\",
+    \"patternDescription\": \"\\u041e\\u0442\\u0432\\u0435\\u0442\\u043d\\u044b\\u0439 \\u0440\\u0435\\u0433\\u0438\\u0441\\u0442\\u0440 \\u0434\\u043b\\u044f \\u043f\\u043e\\u043b\\u0443\\u0447\\u0435\\u043d\\u0438\\u044f \\u043c\\u0430\\u0441\\u0442\\u0435\\u0440 \\u043f\\u0440\\u0430\\u0432 \\u043d\\u0430 \\u0441\\u0435\\u0440\\u0432\\u0435\\u0440\\u0435. \\u0412\\u044b\\u0447\\u0438\\u0441\\u043b\\u044f\\u0435\\u0442\\u0441\\u044f \\u043d\\u0430 \\u0441\\u0442\\u0435\\u043a\\u0435. \\u041f\\u043e\\u0441\\u043b\\u0435 \\u043e\\u0442\\u043f\\u0440\\u0430\\u0432\\u043a\\u0438 \\u0441\\u043e\\u0442\\u0440\\u0451\\u0442\\u0441\\u044f. (Only notify)\",
+    \"structItems\": [
+        {
+            \"structItemPatternBits\": 8,
+            \"structItemPatternId\": 0,
+            \"structItemCount\": 16,
+            \"structItemVarName\": \"serverHashThingSessionKey\",
+            \"structItemPatternType\": \"native\",
+            \"structItemPatternName\": \"Unsigned\",
+            \"structItemDescription\": \"\\u041f\\u043e\\u0432\\u0442\\u043e\\u0440 \\u043e\\u0442\\u0432\\u0435\\u0442\\u0430 \\u0441\\u0435\\u0440\\u0432\\u0435\\u0440\\u0430 \\u043d\\u0430 \\u0432\\u0441\\u044f\\u043a\\u0438\\u0439 \\u0441\\u043b\\u0443\\u0447\\u0430\\u0439. \\u041f\\u043e\\u0441\\u043b\\u0435 \\u043e\\u0442\\u0432\\u0435\\u0442 \\u044d\\u0442\\u043e\\u0442 \\u0445\\u0435\\u0448 \\u0441\\u043e\\u0442\\u0440\\u0451\\u0442\\u0441\\u044f.\",
+            \"structItemDefault\": 0
+        },
+        {
+            \"structItemPatternBits\": 8,
+            \"structItemPatternId\": 0,
+            \"structItemCount\": 16,
+            \"structItemVarName\": \"thingHashServerHash\",
+            \"structItemPatternType\": \"native\",
+            \"structItemPatternName\": \"Unsigned\",
+            \"structItemDescription\": \"\\u0425\\u0435\\u0448 \\u043d\\u0430 \\u043a\\u043b\\u044e\\u0447 \\u0441\\u0435\\u0440\\u0432\\u0435\\u0440\\u0430, \\u0447\\u0442\\u043e\\u0431\\u044b \\u0441\\u0435\\u0440\\u0432\\u0435\\u0440 \\u0434\\u0430\\u043b \\u043c\\u0430\\u0441\\u0442\\u0435\\u0440 \\u043f\\u0440\\u0430\\u0432\\u0430. \\u041f\\u043e\\u0441\\u043b\\u0435 \\u043e\\u0442\\u0432\\u0435\\u0442 \\u044d\\u0442\\u043e\\u0442 \\u0445\\u0435\\u0448 \\u0441\\u043e\\u0442\\u0440\\u0451\\u0442\\u0441\\u044f.\",
+            \"structItemDefault\": 0
+        }
+    ]
+}`}]]);
