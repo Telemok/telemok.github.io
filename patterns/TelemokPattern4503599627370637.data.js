36d1eff50af96eadf669da71dc2f5902bc193679
Telemok <tlevelpack@yandex.ru>
2021-06-13 01:19:49 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370637.data.js
@@ -0,0 +1,31 @@
+/* telemok.com/patterns/pattern4503599627370637/ */
+export var telemokPatternsData = new Map([,
+	[4503599627370637,{json:`{
+    \"patternType\": \"struct\",
+    \"patternVersion\": 1,
+    \"patternBits\": 256,
+    \"patternName\": \"ThingValidateServerKeys\",
+    \"patternDescription\": \"\\u0420\\u0435\\u0433\\u0438\\u0441\\u0442\\u0440 \\u0437\\u0430\\u043f\\u0438\\u0441\\u0438 \\u0432 \\u0432\\u0435\\u0449\\u044c, \\u0447\\u0442\\u043e\\u0431\\u044b \\u0434\\u043e\\u043a\\u0430\\u0437\\u0430\\u0442\\u044c, \\u0447\\u0442\\u043e \\u0441\\u0435\\u0440\\u0432\\u0435\\u0440 \\u0432\\u0435\\u0440\\u043d\\u044b\\u0439. (Only write)\",
+    \"structItems\": [
+        {
+            \"structItemPatternBits\": 8,
+            \"structItemPatternId\": 0,
+            \"structItemCount\": 16,
+            \"structItemVarName\": \"thingSessionKey\",
+            \"structItemPatternType\": \"native\",
+            \"structItemPatternName\": \"Unsigned\",
+            \"structItemDescription\": \"\\u041f\\u043e\\u0432\\u0442\\u043e\\u0440 \\u043f\\u0443\\u0431\\u043b\\u0438\\u0447\\u043d\\u043e\\u0433\\u043e \\u043a\\u043b\\u044e\\u0447\\u0430 \\u0441\\u0435\\u0441\\u0441\\u0438\\u0438 \\u043f\\u0440\\u0438\\u0431\\u043e\\u0440\\u0430.\",
+            \"structItemDefault\": 0
+        },
+        {
+            \"structItemPatternBits\": 8,
+            \"structItemPatternId\": 0,
+            \"structItemCount\": 16,
+            \"structItemVarName\": \"serverHashThingSessionKey\",
+            \"structItemPatternType\": \"native\",
+            \"structItemPatternName\": \"Unsigned\",
+            \"structItemDescription\": \"\\u0425\\u0435\\u0448 \\u0441\\u0435\\u0440\\u0432\\u0435\\u0440\\u0430 \\u043d\\u0430 \\u0441\\u0435\\u0441\\u0441\\u0438\\u044e \\u043f\\u0440\\u0438\\u0431\\u043e\\u0440\\u0430 \\u0434\\u043e\\u043a\\u0430\\u0437\\u044b\\u0432\\u0430\\u0435\\u0442 \\u043f\\u0440\\u0438\\u0431\\u043e\\u0440\\u0443, \\u0447\\u0442\\u043e \\u0441\\u0435\\u0440\\u0432\\u0435\\u0440 \\u0432\\u0435\\u0440\\u043d\\u044b\\u0439.\",
+            \"structItemDefault\": 0
+        }
+    ]
+}`}]]);
