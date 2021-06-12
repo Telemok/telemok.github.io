0892636fdcbf330fd9d37664baf0e5bf1bbe8455
Telemok <tlevelpack@yandex.ru>
2021-06-13 01:19:57 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370508.data.js
@@ -0,0 +1,31 @@
+/* telemok.com/patterns/pattern4503599627370508/ */
+export var telemokPatternsData = new Map([,
+	[4503599627370508,{json:`{
+    \"patternType\": \"struct\",
+    \"patternVersion\": 1,
+    \"patternBits\": 256,
+    \"patternName\": \"Text16x2\",
+    \"patternDescription\": \"1\",
+    \"structItems\": [
+        {
+            \"structItemPatternBits\": 8,
+            \"structItemPatternId\": 0,
+            \"structItemCount\": 16,
+            \"structItemVarName\": \"line0\",
+            \"structItemPatternType\": \"native\",
+            \"structItemPatternName\": \"Unsigned\",
+            \"structItemDescription\": \"Stroka0\",
+            \"structItemDefault\": \"0\"
+        },
+        {
+            \"structItemPatternBits\": 8,
+            \"structItemPatternId\": 0,
+            \"structItemCount\": 16,
+            \"structItemVarName\": \"line1\",
+            \"structItemPatternType\": \"native\",
+            \"structItemPatternName\": \"Unsigned\",
+            \"structItemDescription\": \"Stroka1\",
+            \"structItemDefault\": \"0\"
+        }
+    ]
+}`}]]);
