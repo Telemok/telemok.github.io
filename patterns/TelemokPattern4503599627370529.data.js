84b514172fe39bd41dc85aa8eb5846a6868eccd0
Telemok <tlevelpack@yandex.ru>
2021-06-13 02:52:51 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370529.data.js
@@ -0,0 +1,43 @@
+/* telemok.com/patterns/pattern4503599627370529/ */
+export var telemokPatternsData = new Map([
+	[4503599627370529,{json:`{
+    \"patternVersion\": 1,
+    \"patternName\": \"Pin3Function\",
+    \"patternBits\": 3,
+    \"patternDescription\": \"3-\\u043f\\u0438\\u043d\\u043e\\u0432\\u044b\\u0439 \\u0440\\u0430\\u0437\\u044a\\u0451\\u043c. 5 \\u0432\\u043e\\u043b\\u044c\\u0442\\u0430 - \\u0434\\u0430, GND - \\u043d\\u0435\\u0442.\",
+    \"patternType\": \"enum\",
+    \"enumItems\": [
+        {
+            \"enumItemRaw\": 0,
+            \"enumItemIsDefault\": 1,
+            \"enumItemName\": \"OFF\",
+            \"enumItemDescription\": \"\\u0420\\u0430\\u0437\\u044a\\u0451\\u043c \\u043d\\u0435 \\u0438\\u0441\\u043f\\u043e\\u043b\\u044c\\u0437\\u0443\\u0435\\u0442\\u0441\\u044f.\"
+        },
+        {
+            \"enumItemRaw\": 1,
+            \"enumItemIsDefault\": 1,
+            \"enumItemName\": \"TEN\",
+            \"enumItemDescription\": \"\\u041f\\u043e\\u0434\\u043a\\u043b\\u044e\\u0447\\u0435\\u043d\\u0438\\u0435 \\u0422\\u042d\\u041d\\u0430.\"
+        },
+        {
+            \"enumItemRaw\": 2,
+            \"enumItemIsDefault\": 1,
+            \"enumItemName\": \"pneumatic_blowing\",
+            \"enumItemDescription\": \"\\u0414\\u043e\\u043f\\u043e\\u043b\\u043d\\u0438\\u0442\\u0435\\u043b\\u044c\\u043d\\u0430\\u044f \\u043e\\u0442\\u0434\\u0443\\u0432\\u043a\\u0430 \\u0434\\u043b\\u044f \\u0442\\u043e\\u0440\\u0444\\u0430 \\u0438 \\u0430\\u0433\\u0440\\u043e\\u043f\\u0435\\u043b\\u043b\\u0435\\u0442.\"
+        },
+        {
+            \"enumItemRaw\": 3,
+            \"enumItemIsDefault\": 1,
+            \"enumItemName\": \"PUMP\",
+            \"enumItemDescription\": \"\\u041d\\u0430\\u0441\\u043e\\u0441 \\u043a\\u0430\\u0447\\u0430\\u0435\\u0442 \\u0442\\u0435\\u043f\\u043b\\u043e\\u043d\\u043e\\u0441\\u0438\\u0442\\u0435\\u043b\\u044c, \\u043a\\u043e\\u0433\\u0434\\u0430 \\u0435\\u0441\\u0442\\u044c \\u0433\\u043e\\u0440\\u0435\\u043d\\u0438\\u0435.\"
+        },
+        {
+            \"enumItemRaw\": 4,
+            \"enumItemIsDefault\": 1,
+            \"enumItemName\": \"MAX\",
+            \"enumItemDescription\": \"Max count of enum items. If set - enum undefined.\"
+        }
+    ],
+    \"enumSigned\": 0,
+    \"enumPrefix\": \"Pin3Function_\"
+}`}]]);
