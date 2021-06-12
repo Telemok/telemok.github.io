218c5117e307807b81fdf5c775ca5da4ad57fef7
Telemok <tlevelpack@yandex.ru>
2021-06-13 01:19:53 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370639.data.js
@@ -0,0 +1,21 @@
+/* telemok.com/patterns/pattern4503599627370639/ */
+export var telemokPatternsData = new Map([,
+	[4503599627370639,{json:`{
+    \"patternType\": \"struct\",
+    \"patternVersion\": 1,
+    \"patternBits\": 128,
+    \"patternName\": \"ThingServerPassword\",
+    \"patternDescription\": \"\\u0420\\u0435\\u0433\\u0438\\u0441\\u0442\\u0440, \\u0441\\u0447\\u0438\\u0442\\u044b\\u0432\\u0430\\u0435\\u043c\\u044b\\u0439 \\u0441 \\u0432\\u0435\\u0449\\u0438, \\u0447\\u0442\\u043e\\u0431\\u044b \\u043d\\u0430\\u0447\\u0430\\u0442\\u044c \\u043b\\u043e\\u0433\\u0438\\u043d\\u0438\\u0442\\u044c\\u0441\\u044f \\u043d\\u0430 \\u0441\\u0435\\u0440\\u0432\\u0435\\u0440. \\u041d\\u0435 \\u043c\\u0435\\u043d\\u044f\\u0435\\u0442\\u0441\\u044f \\u0431\\u0435\\u0437 \\u043f\\u0435\\u0440\\u0435\\u0437\\u0430\\u0433\\u0440\\u0443\\u0437\\u043a\\u0438 \\u0432\\u0435\\u0449\\u0438\",
+    \"structItems\": [
+        {
+            \"structItemPatternBits\": 8,
+            \"structItemPatternId\": 0,
+            \"structItemCount\": 16,
+            \"structItemVarName\": \"thingServerPassword\",
+            \"structItemPatternType\": \"native\",
+            \"structItemPatternName\": \"Unsigned\",
+            \"structItemDescription\": \"\\u041d\\u0435\\u0438\\u0437\\u043c\\u0435\\u043d\\u044f\\u0435\\u043c\\u044b\\u0439 \\u043f\\u0430\\u0440\\u043e\\u043b\\u044c \\u0432\\u0435\\u0449\\u0438. \\u041d\\u0443\\u0436\\u0435\\u043d \\u0434\\u043b\\u044f \\u043b\\u043e\\u0433\\u0438\\u043d\\u0430, \\u0447\\u0442\\u043e\\u0431\\u044b \\u0437\\u0430\\u0449\\u0438\\u0442\\u0438\\u0442\\u044c \\u0441\\u0435\\u0440\\u0432\\u0435\\u0440 \\u043e\\u0442 \\u0441\\u043a\\u0430\\u043d\\u0438\\u0440\\u043e\\u0432\\u0430\\u043d\\u0438\\u044f \\u0441\\u0435\\u0440\\u0438\\u0439\\u043d\\u0438\\u043a\\u043e\\u0432. \\u041d\\u0435 \\u0434\\u0430\\u0451\\u0442 \\u043f\\u043e\\u0432\\u044b\\u0448\\u0435\\u043d\\u0438\\u044f \\u043f\\u0440\\u0430\\u0432. client_password for Oauth.\",
+            \"structItemDefault\": 0
+        }
+    ]
+}`}]]);
