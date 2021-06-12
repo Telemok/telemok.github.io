6e36c6cb3f43c3efa9cee9efce7e0a9acaadf1b9
Telemok <tlevelpack@yandex.ru>
2021-06-13 02:52:54 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370527.data.js
@@ -0,0 +1,55 @@
+/* telemok.com/patterns/pattern4503599627370527/ */
+export var telemokPatternsData = new Map([
+	[4503599627370527,{json:`{
+    \"patternVersion\": 1,
+    \"patternName\": \"Pin6Function\",
+    \"patternBits\": 3,
+    \"patternDescription\": \"\\u0412\\u044b\\u0431\\u043e\\u0440 \\u0444\\u0443\\u043d\\u043a\\u0446\\u0438\\u0438 USART \\u043f\\u043e\\u0440\\u0442\\u0430 \\u043f\\u0435\\u043b\\u043b\\u0435\\u0442\\u043d\\u043e\\u0439 \\u0433\\u043e\\u0440\\u0435\\u043b\\u043a\\u0438.\",
+    \"patternType\": \"enum\",
+    \"enumItems\": [
+        {
+            \"enumItemRaw\": 0,
+            \"enumItemIsDefault\": 1,
+            \"enumItemName\": \"OFF\",
+            \"enumItemDescription\": \"\\u041f\\u043e\\u0440\\u0442 \\u0432\\u044b\\u043b\\u044e\\u0447\\u0435\\u043d \\u0438 \\u043d\\u0435 \\u0440\\u0435\\u0430\\u0433\\u0438\\u0440\\u0443\\u0435\\u0442 \\u043d\\u0430 \\u0432\\u043d\\u0435\\u0448\\u043d\\u0435\\u0435 \\u0432\\u043e\\u0437\\u0434\\u0435\\u0439\\u0441\\u0442\\u0432\\u0438\\u0435.\"
+        },
+        {
+            \"enumItemRaw\": 1,
+            \"enumItemIsDefault\": 1,
+            \"enumItemName\": \"GSM\",
+            \"enumItemDescription\": \"TX, RX, Power \\u0438\\u0441\\u043f\\u043e\\u043b\\u044c\\u0437\\u0443\\u044e\\u0442\\u0441\\u044f \\u0434\\u043b\\u044f GSM.\"
+        },
+        {
+            \"enumItemRaw\": 2,
+            \"enumItemIsDefault\": 1,
+            \"enumItemName\": \"NBIOT\",
+            \"enumItemDescription\": \"TX, RX, Power \\u0438\\u0441\\u043f\\u043e\\u043b\\u044c\\u0437\\u0443\\u044e\\u0442\\u0441\\u044f \\u0434\\u043b\\u044f NBIOT.\"
+        },
+        {
+            \"enumItemRaw\": 3,
+            \"enumItemIsDefault\": 1,
+            \"enumItemName\": \"USART\",
+            \"enumItemDescription\": \"Usart \\u043a\\u043e\\u043c\\u0430\\u043d\\u0434\\u044b \\u043a\\u0430\\u043a \\u0432 SMS.\"
+        },
+        {
+            \"enumItemRaw\": 4,
+            \"enumItemIsDefault\": 1,
+            \"enumItemName\": \"TELEMOK\",
+            \"enumItemDescription\": \"\\u041f\\u043e\\u0434\\u043a\\u043b\\u044e\\u0447\\u0435\\u043d\\u0438\\u0435 \\u043f\\u043b\\u0430\\u043d\\u0448\\u0435\\u0442\\u0430 \\u043f\\u043e \\u043f\\u0440\\u043e\\u0442\\u043e\\u043a\\u043e\\u043b\\u0443 telemok.com\"
+        },
+        {
+            \"enumItemRaw\": 5,
+            \"enumItemIsDefault\": 1,
+            \"enumItemName\": \"AUTO\",
+            \"enumItemDescription\": \"\\u041f\\u043e\\u043f\\u044b\\u0442\\u043a\\u0430 \\u0430\\u0432\\u0442\\u043e\\u043c\\u0430\\u0442\\u0438\\u0447\\u0435\\u0441\\u043a\\u0438 \\u043e\\u043f\\u0440\\u0435\\u0434\\u0435\\u043b\\u0438\\u0442\\u044c \\u0442\\u0438\\u043f \\u043f\\u043e\\u0434\\u043a\\u043b\\u044e\\u0447\\u0435\\u043d\\u0438\\u044f.\"
+        },
+        {
+            \"enumItemRaw\": 6,
+            \"enumItemIsDefault\": 1,
+            \"enumItemName\": \"MAX\",
+            \"enumItemDescription\": \"Max count of enum items. If set - enum undefined.\"
+        }
+    ],
+    \"enumSigned\": 0,
+    \"enumPrefix\": \"Pin6Function_\"
+}`}]]);
