107fb938f562b9c66409a3265b81caeb9806a9c9
Telemok <tlevelpack@yandex.ru>
2021-06-17 21:57:04 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370497.data.js
@@ -0,0 +1,47 @@
+/* telemok.com/patterns/pattern4503599627370497/ */
+export var telemokPatternsData = new Map([
+	[4503599627370497,{json:`{
+    \"patternType\": \"enum\",
+    \"patternBits\": 3,
+    \"patternName\": \"BurnAlgorithm\",
+    \"patternVersion\": 1,
+    \"patternDescription\": \"generated enum\",
+    \"enumPrefix\": \"BurnAlgorithm_\",
+    \"enumItems\": [
+        {
+            \"enumItemRaw\": 0,
+            \"enumItemName\": \"MINIMAL\",
+            \"enumItemDescription\": \"emergency mode\"
+        },
+        {
+            \"enumItemRaw\": 1,
+            \"enumItemName\": \"FIX_SUPPLY\",
+            \"enumItemDescription\": \"\"
+        },
+        {
+            \"enumItemRaw\": 2,
+            \"enumItemName\": \"FIX_SUPPLY_FIX_FAN\",
+            \"enumItemDescription\": \"\"
+        },
+        {
+            \"enumItemRaw\": 3,
+            \"enumItemName\": \"T_IN_HYSTERESIS\",
+            \"enumItemDescription\": \"\"
+        },
+        {
+            \"enumItemRaw\": 4,
+            \"enumItemName\": \"T_IN_SMOOTH\",
+            \"enumItemDescription\": \"\"
+        },
+        {
+            \"enumItemRaw\": 5,
+            \"enumItemName\": \"T_IN_AND_STREET_SMOOTH\",
+            \"enumItemDescription\": \"need street termometer\"
+        },
+        {
+            \"enumItemRaw\": 6,
+            \"enumItemName\": \"T_ROOM_SMOOTH\",
+            \"enumItemDescription\": \"need room termometer\"
+        }
+    ]
+}`}]]);
