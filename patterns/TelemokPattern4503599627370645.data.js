58c219ce699305179d29c214f7ea27119b8f180e
Telemok <tlevelpack@yandex.ru>
2021-07-06 10:54:08 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370645.data.js
@@ -0,0 +1,19 @@
+/* telemok.com/patterns/pattern4503599627370645/ */
+export var telemokPatternsData = new Map([
+	[4503599627370645,{json:`{
+    \"patternVersion\": 1,
+    \"patternName\": \"testQoutesTilda\",
+    \"patternBits\": 1,
+    \"patternDescription\": \"Quote double \\`\",
+    \"patternType\": \"enum\",
+    \"enumRawDefault\": 0,
+    \"enumSigned\": 0,
+    \"enumPrefix\": \"\",
+    \"enumItems\": [
+        {
+            \"enumItemRaw\": 0,
+            \"enumItemName\": \"UPPERCASE_ENUM_NAME_0\",
+            \"enumItemDescription\": \"Description: enum item 0.\"
+        }
+    ]
+}`}]]);
