602d8980befbd753d9f1bdbb6ece372e218a3f78
Telemok <tlevelpack@yandex.ru>
2021-10-15 14:02:22 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370609.data.js
@@ -0,0 +1,91 @@
+/* telemok.com/patterns/pattern4503599627370609/ */
+export var telemokPatternsData = new Map([
+	[4503599627370609,{json:`{
+    "patternType": "struct",
+    "patternVersion": 1,
+    "patternBits": 512,
+    "patternName": "BlStartBlock",
+    "patternDescription": "\\u0421\\u0442\\u0440\\u0443\\u043a\\u0442\\u0443\\u0440\\u0430 \\u043d\\u0430\\u0447\\u0438\\u043d\\u0430\\u044e\\u0449\\u0430\\u044f \\u043f\\u0440\\u043e\\u0448\\u0438\\u0432\\u043a\\u0443 \\u043f\\u0440\\u043e\\u0433\\u0440\\u0430\\u043c\\u043c\\u044b",
+    "structItems": [
+        {
+            "structItemPatternBits": 8,
+            "structItemPatternId": 0,
+            "structItemCount": 16,
+            "structItemVarName": "device_type",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "\\u041d\\u0430\\u0437\\u0432\\u0430\\u043d\\u0438\\u0435 \\u0443\\u0441\\u0442\\u0440\\u043e\\u0439\\u0441\\u0442\\u0432\\u0430",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 8,
+            "structItemPatternId": 0,
+            "structItemCount": 16,
+            "structItemVarName": "BL_name",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "\\u041d\\u0430\\u0437\\u0432\\u0430\\u043d\\u0438\\u0435 \\u0443\\u0441\\u0442\\u0440\\u043e\\u0439\\u0441\\u0442\\u0432\\u0430",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 8,
+            "structItemPatternId": 0,
+            "structItemCount": 16,
+            "structItemVarName": "FW_name",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "\\u041d\\u0430\\u0437\\u0432\\u0430\\u043d\\u0438\\u0435 \\u0443\\u0441\\u0442\\u0440\\u043e\\u0439\\u0441\\u0442\\u0432\\u0430",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 53,
+            "structItemPatternId": 0,
+            "structItemCount": 1,
+            "structItemVarName": "SN",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "\\u0421\\u0435\\u0440\\u0438\\u0439\\u043d\\u044b\\u0439 \\u043d\\u043e\\u043c\\u0435\\u0440",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 11,
+            "structItemPatternId": 0,
+            "structItemCount": 1,
+            "structItemVarName": "reserved1",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "\\u0417\\u0430\\u0433\\u043b\\u0443\\u0448\\u043a\\u0430",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 32,
+            "structItemPatternId": 0,
+            "structItemCount": 1,
+            "structItemVarName": "lengthBytes",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "\\u041c\\u0430\\u043a\\u0441\\u0438\\u043c\\u0430\\u043b\\u044c\\u043d\\u044b\\u0439 \\u0440\\u0430\\u0437\\u043c\\u0435\\u0440 122624",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 16,
+            "structItemPatternId": 0,
+            "structItemCount": 1,
+            "structItemVarName": "fwCrc16",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "\\u0421\\u0443\\u043c\\u043c\\u0430\\u0440\\u043d\\u043e\\u0435 crc16 \\u043e\\u0442 \\u0432\\u0441\\u0435\\u0445 \\u0431\\u0430\\u0439\\u0442\\u043e\\u0432 \\u0441\\u0430\\u043c\\u043e\\u0439 \\u043f\\u0440\\u043e\\u0448\\u0438\\u0432\\u043a\\u0438 \\u0441 \\u0434\\u0438\\u043d\\u0430\\u043c\\u0438\\u0447\\u0435\\u0441\\u043a\\u043e\\u0439 \\u0441\\u0442\\u0440\\u0430\\u043d\\u0438\\u0446\\u0435\\u0439.",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 16,
+            "structItemPatternId": 0,
+            "structItemCount": 1,
+            "structItemVarName": "reservedCrc16ThisBlock",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "Unsigned native value",
+            "structItemDefault": 0
+        }
+    ]
+}`}]]);
