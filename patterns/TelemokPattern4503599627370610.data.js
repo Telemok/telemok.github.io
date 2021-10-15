e8dd03773b404fa7598f88227fba6fde5ba140fc
Telemok <tlevelpack@yandex.ru>
2021-10-15 14:02:13 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370610.data.js
@@ -0,0 +1,51 @@
+/* telemok.com/patterns/pattern4503599627370610/ */
+export var telemokPatternsData = new Map([
+	[4503599627370610,{json:`{
+    "patternType": "struct",
+    "patternVersion": 1,
+    "patternBits": 4,
+    "patternName": "BlOrders",
+    "patternDescription": "",
+    "structItems": [
+        {
+            "structItemPatternBits": 1,
+            "structItemPatternId": 0,
+            "structItemCount": 1,
+            "structItemVarName": "exit",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Bool",
+            "structItemDescription": "Boolean native value",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 1,
+            "structItemPatternId": 0,
+            "structItemCount": 1,
+            "structItemVarName": "stop",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Bool",
+            "structItemDescription": "Boolean native value",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 1,
+            "structItemPatternId": 0,
+            "structItemCount": 1,
+            "structItemVarName": "start",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Bool",
+            "structItemDescription": "Boolean native value",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 1,
+            "structItemPatternId": 0,
+            "structItemCount": 1,
+            "structItemVarName": "erase",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Bool",
+            "structItemDescription": "Boolean native value",
+            "structItemDefault": 0
+        }
+    ]
+}`}]]);
