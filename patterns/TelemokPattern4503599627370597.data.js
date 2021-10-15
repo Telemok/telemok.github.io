02b300af8df909130c3dadd4a4663df0039bad99
Telemok <tlevelpack@yandex.ru>
2021-10-15 14:02:45 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370597.data.js
@@ -0,0 +1,44 @@
+/* telemok.com/patterns/pattern4503599627370597/ */
+export var telemokPatternsData = new Map([
+	[4503599627370597,{json:`{
+    "patternVersion": 1,
+    "patternName": "BlStatus",
+    "patternBits": 3,
+    "patternDescription": "\\u0422\\u0435\\u043a\\u0443\\u0449\\u0438\\u0439 \\u0441\\u0442\\u0430\\u0442\\u0443\\u0441 \\u0437\\u0430\\u0433\\u0440\\u0443\\u0437\\u0447\\u0438\\u043a\\u0430.",
+    "patternType": "enum",
+    "enumRawDefault": 0,
+    "enumSigned": 0,
+    "enumPrefix": "BlStatus_",
+    "enumItems": [
+        {
+            "enumItemRaw": 0,
+            "enumItemName": "READY",
+            "enumItemDescription": "\\u0412\\u043a\\u043b\\u044e\\u0447\\u0438\\u043b\\u043e\\u0441\\u044c. \\u041d\\u0438\\u0447\\u0435\\u0433\\u043e \\u043d\\u0435 \\u043f\\u0440\\u043e\\u0438\\u0441\\u0445\\u043e\\u0434\\u0438\\u0442."
+        },
+        {
+            "enumItemRaw": 1,
+            "enumItemName": "UPDATING",
+            "enumItemDescription": "\\u0421\\u0435\\u0439\\u0447\\u0430\\u0441 \\u0438\\u0434\\u0451\\u0442 \\u043e\\u0431\\u043d\\u043e\\u0432\\u043b\\u0435\\u043d\\u0438\\u0435. \\u0417\\u0430\\u0433\\u0440\\u0443\\u0437\\u0447\\u0438\\u043a \\u0448\\u043b\\u0451\\u0442 \\u0441\\u0442\\u0430\\u0442\\u0443\\u0441."
+        },
+        {
+            "enumItemRaw": 2,
+            "enumItemName": "PAUSED",
+            "enumItemDescription": "\\u041e\\u0431\\u043d\\u043e\\u0432\\u043b\\u0435\\u043d\\u0438\\u0435 \\u043f\\u0440\\u0438\\u043e\\u0441\\u0442\\u0430\\u043d\\u043e\\u0432\\u043b\\u0435\\u043d\\u043e. \\u0417\\u0430\\u0433\\u0440\\u0443\\u0437\\u0447\\u0438\\u043a \\u043d\\u0435 \\u0448\\u043b\\u0451\\u0442 \\u0441\\u0442\\u0430\\u0442\\u0443\\u0441."
+        },
+        {
+            "enumItemRaw": 3,
+            "enumItemName": "CHECKSUMM_ERROR",
+            "enumItemDescription": "\\u0421\\u0443\\u043c\\u043c\\u0430\\u0440\\u043d\\u044b\\u0439 \\u0445\\u0435\\u0448 \\u043f\\u0440\\u043e\\u0448\\u0438\\u0432\\u043a\\u0438 \\u043d\\u0435 \\u0441\\u043e\\u0448\\u0451\\u043b\\u0441\\u044f."
+        },
+        {
+            "enumItemRaw": 4,
+            "enumItemName": "COMPLETE",
+            "enumItemDescription": "\\u0412\\u0441\\u0451 \\u043f\\u0440\\u043e\\u0448\\u0438\\u043b\\u043e\\u0441\\u044c."
+        },
+        {
+            "enumItemRaw": 5,
+            "enumItemName": "NOT_READY",
+            "enumItemDescription": "\\u041d\\u0435 \\u0433\\u043e\\u0442\\u043e\\u0432\\u043e \\u043a \\u043f\\u0440\\u043e\\u0448\\u0438\\u0432\\u043a\\u0435."
+        }
+    ]
+}`}]]);
