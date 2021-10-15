bd90d6bfcd00658f70d237d527f9e6685bf17f82
Telemok <tlevelpack@yandex.ru>
2021-10-15 14:02:42 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370607.data.js
@@ -0,0 +1,21 @@
+/* telemok.com/patterns/pattern4503599627370607/ */
+export var telemokPatternsData = new Map([
+	[4503599627370607,{json:`{
+    "patternType": "struct",
+    "patternVersion": 1,
+    "patternBits": 128,
+    "patternName": "Uint8x16",
+    "patternDescription": "16 fields of 8 bits = 128 bits",
+    "structItems": [
+        {
+            "structItemPatternBits": 8,
+            "structItemPatternId": 0,
+            "structItemCount": 16,
+            "structItemVarName": "data",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "any 16 bytes",
+            "structItemDefault": 0
+        }
+    ]
+}`}]]);
