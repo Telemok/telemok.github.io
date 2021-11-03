2afe6ab8978d6d20f08f0ed14ccef0a5610d4aed
Telemok <tlevelpack@yandex.ru>
2021-11-03 12:15:27 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370568.data.js
@@ -0,0 +1,21 @@
+/* telemok.com/patterns/pattern4503599627370568/ */
+export var telemokPatternsData = new Map([
+	[4503599627370568,{json:`{
+    "patternType": "struct",
+    "patternVersion": 1,
+    "patternBits": 128,
+    "patternName": "SessionUint8x16",
+    "patternDescription": "16-\\u0431\\u0430\\u0439\\u0442\\u043d\\u044b\\u0439 \\u043a\\u043b\\u044e\\u0447, \\u0433\\u0435\\u043d\\u0435\\u0440\\u0438\\u0440\\u0443\\u0435\\u043c\\u044b\\u0439 \\u043a\\u0430\\u0436\\u0434\\u044b\\u0439 \\u0440\\u0430\\u0437 \\u0437\\u0430\\u043d\\u043e\\u0432\\u043e \\u043f\\u0440\\u0438 \\u0432\\u043a\\u043b\\u044e\\u0447\\u0435\\u043d\\u0438\\u0438 \\u0443\\u0441\\u0442\\u0440\\u043e\\u0439\\u0441\\u0442\\u0432\\u0430",
+    "structItems": [
+        {
+            "structItemPatternBits": 8,
+            "structItemPatternId": 0,
+            "structItemCount": 16,
+            "structItemVarName": "data",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "\\u0441\\u043b\\u0443\\u0447\\u0430\\u0439\\u043d\\u044b\\u0435 16 \\u0431\\u0430\\u0439\\u0442 \\u0438\\u043b\\u0438 128 \\u0431\\u0438\\u0442",
+            "structItemDefault": 0
+        }
+    ]
+}`}]]);
