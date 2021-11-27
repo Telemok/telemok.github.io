f5ef97127c9a49bb2c04197ac83dfbac9652f3b9
Telemok <tlevelpack@yandex.ru>
2021-11-28 01:36:22 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370666.data.js
@@ -0,0 +1,34 @@
+/* telemok.com/patterns/pattern4503599627370666/ */
+export var telemokPatternsData = new Map([
+	[4503599627370666,{json:`{
+    "patternVersion": 1,
+    "patternName": "RelayAshFunction",
+    "patternBits": 2,
+    "patternDescription": "",
+    "patternType": "enum",
+    "enumRawDefault": 0,
+    "enumSigned": 0,
+    "enumPrefix": "RelayAshFunction_",
+    "enumItems": [
+        {
+            "enumItemRaw": 0,
+            "enumItemName": "OFF",
+            "enumItemDescription": "\\u0420\\u0435\\u043b\\u0435 \\u0437\\u043e\\u043b\\u043e\\u0443\\u0434\\u0430\\u043b\\u0435\\u043d\\u0438\\u044f \\u043d\\u0435 \\u0438\\u0441\\u043f\\u043e\\u043b\\u044c\\u0437\\u0443\\u0435\\u0442\\u0441\\u044f"
+        },
+        {
+            "enumItemRaw": 1,
+            "enumItemName": "ASH_REMOVER",
+            "enumItemDescription": "\\u0420\\u0435\\u043b\\u0435 \\u0432\\u043a\\u043b\\u044e\\u0447\\u0430\\u0435\\u0442 \\u0437\\u043e\\u043b\\u043e\\u0443\\u0434\\u0430\\u043b\\u0435\\u043d\\u0438\\u0435"
+        },
+        {
+            "enumItemRaw": 2,
+            "enumItemName": "PUMP_SECONDARY",
+            "enumItemDescription": "\\u0420\\u0435\\u043b\\u0435 \\u0432\\u043a\\u043b\\u044e\\u0447\\u0430\\u0435\\u0442 \\u043d\\u0430\\u0441\\u043e\\u0441 \\u0434\\u043e\\u043f\\u043e\\u043b\\u043d\\u0438\\u0442\\u0435\\u043b\\u044c\\u043d\\u043e\\u0433\\u043e \\u043a\\u043e\\u043d\\u0442\\u0443\\u0440\\u0430"
+        },
+        {
+            "enumItemRaw": 3,
+            "enumItemName": "RESERVED",
+            "enumItemDescription": "\\u041d\\u0435 \\u043e\\u043f\\u0440\\u0435\\u0434\\u0435\\u043b\\u0451\\u043d\\u043d\\u0430\\u044f \\u0444\\u0443\\u043d\\u043a\\u0446\\u0438\\u044f."
+        }
+    ]
+}`}]]);
