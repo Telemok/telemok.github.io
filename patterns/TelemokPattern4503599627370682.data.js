6c9c2897e1151ae05d8e19acf931c74e5cdc67f8
Telemok <tlevelpack@yandex.ru>
2021-12-30 15:50:16 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370682.data.js
@@ -0,0 +1,51 @@
+/* telemok.com/patterns/pattern4503599627370682/ */
+export var telemokPatternsData = new Map([
+	[4503599627370682,{json:`{
+    "patternType": "struct",
+    "patternVersion": 1,
+    "patternBits": 128,
+    "patternName": "Karat307Testx",
+    "patternDescription": "\\u041f\\u0440\\u043e\\u0431\\u043d\\u044b\\u0439 \\u0448\\u0430\\u0431\\u043b\\u043e\\u043d, \\u0447\\u0442\\u043e\\u0431\\u044b \\u043e\\u0446\\u0435\\u043d\\u0438\\u0442\\u044c \\u0432\\u043e\\u0437\\u043c\\u043e\\u0436\\u043d\\u043e\\u0441\\u0442\\u044c \\u0445\\u0440\\u0430\\u043d\\u0435\\u043d\\u0438\\u044f \\u0434\\u0430\\u043d\\u043d\\u044b\\u0445 \\u0441 \\u041a\\u0410\\u0420\\u0410\\u0422-307",
+    "structItems": [
+        {
+            "structItemPatternBits": 32,
+            "structItemPatternId": 0,
+            "structItemCount": 1,
+            "structItemVarName": "termo0_out",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 32,
+            "structItemPatternId": 0,
+            "structItemCount": 1,
+            "structItemVarName": "flow0",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 32,
+            "structItemPatternId": 0,
+            "structItemCount": 1,
+            "structItemVarName": "pressure0",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 32,
+            "structItemPatternId": 0,
+            "structItemCount": 1,
+            "structItemVarName": "termo0_in",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "",
+            "structItemDefault": 0
+        }
+    ]
+}`}]]);
