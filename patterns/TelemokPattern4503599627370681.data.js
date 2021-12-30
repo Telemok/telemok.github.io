581bbd719b8b53b8a7a6b6b93f3262a4b3ce47ab
Telemok <tlevelpack@yandex.ru>
2021-12-30 14:34:36 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370681.data.js
@@ -0,0 +1,41 @@
+/* telemok.com/patterns/pattern4503599627370681/ */
+export var telemokPatternsData = new Map([
+	[4503599627370681,{json:`{
+    "patternType": "struct",
+    "patternVersion": 1,
+    "patternBits": 576,
+    "patternName": "Karat307Test",
+    "patternDescription": "\\u041f\\u0440\\u043e\\u0431\\u043d\\u044b\\u0439 \\u0448\\u0430\\u0431\\u043b\\u043e\\u043d, \\u0447\\u0442\\u043e\\u0431\\u044b \\u043e\\u0446\\u0435\\u043d\\u0438\\u0442\\u044c \\u0432\\u043e\\u0437\\u043c\\u043e\\u0436\\u043d\\u043e\\u0441\\u0442\\u044c \\u0445\\u0440\\u0430\\u043d\\u0435\\u043d\\u0438\\u044f \\u0434\\u0430\\u043d\\u043d\\u044b\\u0445 \\u0441 \\u041a\\u0410\\u0420\\u0410\\u0422-307",
+    "structItems": [
+        {
+            "structItemPatternBits": 32,
+            "structItemPatternId": 0,
+            "structItemCount": 6,
+            "structItemVarName": "temperatures",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "RAW \\u043f\\u043e\\u043a\\u0430\\u0437\\u0430\\u043d\\u0438\\u044f \\u0441 6 \\u0434\\u0430\\u0442\\u0447\\u0438\\u043a\\u043e\\u0432 \\u0442\\u0435\\u043c\\u043f\\u0435\\u0440\\u0430\\u0442\\u0443\\u0440\\u044b \\u041a\\u0410\\u0420\\u0410\\u0422-307",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 32,
+            "structItemPatternId": 0,
+            "structItemCount": 6,
+            "structItemVarName": "flow",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "RAW \\u043f\\u043e\\u043a\\u0430\\u0437\\u0430\\u043d\\u0438\\u044f \\u0441 6 \\u0434\\u0430\\u0442\\u0447\\u0438\\u043a\\u043e\\u0432 \\u043f\\u0440\\u043e\\u0442\\u043e\\u043a\\u0430 \\u041a\\u0410\\u0420\\u0410\\u0422-307",
+            "structItemDefault": 0
+        },
+        {
+            "structItemPatternBits": 32,
+            "structItemPatternId": 0,
+            "structItemCount": 6,
+            "structItemVarName": "pressure",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Unsigned",
+            "structItemDescription": "RAW \\u043f\\u043e\\u043a\\u0430\\u0437\\u0430\\u043d\\u0438\\u044f \\u0441 6 \\u0434\\u0430\\u0442\\u0447\\u0438\\u043a\\u043e\\u0432 \\u0434\\u0430\\u0432\\u043b\\u0435\\u043d\\u0438\\u044f \\u041a\\u0410\\u0420\\u0410\\u0422-307",
+            "structItemDefault": 0
+        }
+    ]
+}`}]]);
