b839de7b5bbf06d465e467c7522e02542c1faab8
Telemok <tlevelpack@yandex.ru>
2022-01-25 16:30:28 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370690.data.js
@@ -0,0 +1,39 @@
+/* telemok.com/patterns/pattern4503599627370690/ */
+export var telemokPatternsData = new Map([
+	[4503599627370690,{json:`{
+    "patternType": "singular",
+    "patternVersion": 1,
+    "patternBits": 49,
+    "patternName": "TelemokRtcTimestamp",
+    "patternDescription": "RTC - real time clock of Microcontroller based on 32768 hz quartz.",
+    "singularSigned": false,
+    "singularEnum": [
+        {
+            "enumItemRaw": 562949953421310,
+            "enumItemName": "NOT_SUPPORTED",
+            "enumItemDescription": "RTC is absent and can not be used."
+        },
+        {
+            "enumItemRaw": 562949953421309,
+            "enumItemName": "DISABLED",
+            "enumItemDescription": "RTC is disabled, but can be enabled."
+        },
+        {
+            "enumItemRaw": 562949953421308,
+            "enumItemName": "ENABLED",
+            "enumItemDescription": "Order to enable RTC without change."
+        },
+        {
+            "enumItemRaw": 562949953421308,
+            "enumItemName": "UNKNOWN",
+            "enumItemDescription": "Unknown value"
+        }
+    ],
+    "singularCalc": [
+        {
+            "calcItemOperation": "DIV",
+            "calcItemValue": "32768",
+            "calcItemDescription": "Result is seconds. Divide for quartz 32768hz."
+        }
+    ]
+}`}]]);
