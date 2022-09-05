1e9505f0c423bd392932355caa01e799d0d8c6fe
Telemok <tlevelpack@yandex.ru>
2022-09-05 13:36:37 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370723.data.js
@@ -0,0 +1,23 @@
+/* telemok.com/patterns/pattern4503599627370723/ */
+export var telemokPatternsData = new Map([
+	[4503599627370723,{json:`{
+    "patternType": "struct",
+    "patternVersion": 1,
+    "patternBits": 1,
+    "patternName": "OrdersOneTimeService",
+    "patternDescription": "",
+    "structItems": [
+        {
+            "structItemPatternBits": 1,
+            "structItemPatternId": 0,
+            "structItemCount": 1,
+            "structItemVarName": "startMeasureBatteryReliability",
+            "structItemPatternType": "native",
+            "structItemPatternName": "Bool",
+            "structItemDescription": "",
+            "structItemDefault": "0",
+            "structItemMerge": "or",
+            "structItemArrayType": "NONE"
+        }
+    ]
+}`}]]);
