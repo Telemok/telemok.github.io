3ce2c1317b0a65240d16b20eacc36afaeb014146
Telemok <tlevelpack@yandex.ru>
2022-09-05 13:58:12 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370724.data.js
@@ -0,0 +1,39 @@
+/* telemok.com/patterns/pattern4503599627370724/ */
+export var telemokPatternsData = new Map([
+	[4503599627370724,{json:`{
+    "patternVersion": 1,
+    "patternName": "WhyCantMeasure12vFall",
+    "patternBits": 3,
+    "patternDescription": "",
+    "patternType": "enum",
+    "enumRawDefault": 0,
+    "enumSigned": 0,
+    "enumPrefix": "MEAS_12V_FALL_REASON_",
+    "enumItems": [
+        {
+            "enumItemRaw": 0,
+            "enumItemName": "CAN_MEASURE",
+            "enumItemDescription": "All is good, can measure."
+        },
+        {
+            "enumItemRaw": 1,
+            "enumItemName": "TIMEOUT_BOOT",
+            "enumItemDescription": "Protect resistor from overheating if enabled with hot resistor."
+        },
+        {
+            "enumItemRaw": 2,
+            "enumItemName": "TIMEOUT_COOLING",
+            "enumItemDescription": "Protect resistor from overheating after working."
+        },
+        {
+            "enumItemRaw": 3,
+            "enumItemName": "LOW_BATTERY_CHARGE",
+            "enumItemDescription": "Battery not charged. Can not measure voltage down."
+        },
+        {
+            "enumItemRaw": 4,
+            "enumItemName": "NO_220V",
+            "enumItemDescription": "If no 220 vold, cant measure battery reability"
+        }
+    ]
+}`}]]);
