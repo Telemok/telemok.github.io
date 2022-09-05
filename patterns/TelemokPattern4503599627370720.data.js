14146ef5c11494947a32194ee83691b8371982cd
Telemok <tlevelpack@yandex.ru>
2022-09-05 13:17:00 +0700

    Automated update

---
+++ patterns/TelemokPattern4503599627370720.data.js
@@ -0,0 +1,34 @@
+/* telemok.com/patterns/pattern4503599627370720/ */
+export var telemokPatternsData = new Map([
+	[4503599627370720,{json:`{
+    "patternVersion": 1,
+    "patternName": "WhyCantMeasure12vFall",
+    "patternBits": 2,
+    "patternDescription": "",
+    "patternType": "enum",
+    "enumRawDefault": 0,
+    "enumSigned": 0,
+    "enumPrefix": "MEAS_12V_FALL_REASON_",
+    "enumItems": [
+        {
+            "enumItemRaw": 0,
+            "enumItemName": "CAN_MEASURE",
+            "enumItemDescription": "All is good, can measure."
+        },
+        {
+            "enumItemRaw": 1,
+            "enumItemName": "TIMEOUT_BOOT",
+            "enumItemDescription": "Protect resistor from overheating if enabled with hot resistor."
+        },
+        {
+            "enumItemRaw": 2,
+            "enumItemName": "TIMEOUT_COOLING",
+            "enumItemDescription": "Protect resistor from overheating after working."
+        },
+        {
+            "enumItemRaw": 3,
+            "enumItemName": "LOW_BATTERY_CHARGE",
+            "enumItemDescription": "Battery not charged. Can not measure voltage down."
+        }
+    ]
+}`}]]);
